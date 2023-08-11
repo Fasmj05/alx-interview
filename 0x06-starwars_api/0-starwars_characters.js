80bc113fbd4818e9e5d1a6b9717244c65acbb1ac
Fasmj05 <111164432+Fasmj05@users.noreply.github.com>
2023-08-11 01:55:57 +0100

    0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,42 +1,36 @@
 #!/usr/bin/node
+const argv = process.argv;
+const urlFilm = 'https://swapi-api.hbtn.io/api/films/';
+const urlMovie = `${urlFilm}${argv[2]}/`;
 
 const request = require('request');
-const film = process.argv[2] + '/';
 
-const endpoints = {
-  people: 'https://swapi-api.hbtn.io/api/people/',
-  planets: 'https://swapi-api.hbtn.io/api/planets/',
-  films: 'https://swapi-api.hbtn.io/api/films/',
-  species: 'https://swapi-api.hbtn.io/api/species/',
-  vehicles: 'https://swapi-api.hbtn.io/api/vehicles/',
-  starships: 'https://swapi-api.hbtn.io/api/starships/'
-};
+request(urlMovie, function (error, response, body) {
+  if (error == null) {
+    const fbody = JSON.parse(body);
+    const characters = fbody.characters;
 
-const url = endpoints.films + film;
-
-const payload = {
-  url: url,
-  method: 'GET'
-};
-
-request(payload, async (err, res, data) => {
-  if (err) return;
-
-  data = JSON.parse(data);
-  const characters = data.characters;
-
-  const makePromise = (url) => {
-    return new Promise(function (resolve, reject) {
-      request(url, (err, res, data) => {
-        if (err) reject(err);
-        else resolve(data);
-      });
-    });
-  };
-
-  for (const character in characters) {
-    const res = await makePromise(characters[character]);
-    const data = JSON.parse(res);
-    console.log(data.name);
+    if (characters && characters.length > 0) {
+      const limit = characters.length;
+      CharRequest(0, characters[0], characters, limit);
+    }
+  } else {
+    console.log(error);
   }
 });
+
+function CharRequest (idx, url, characters, limit) {
+  if (idx === limit) {
+    return;
+  }
+  request(url, function (error, response, body) {
+    if (!error) {
+      const rbody = JSON.parse(body);
+      console.log(rbody.name);
+      idx++;
+      CharRequest(idx, characters[idx], characters, limit);
+    } else {
+      console.error('error:', error);
+    }
+  });
+}
